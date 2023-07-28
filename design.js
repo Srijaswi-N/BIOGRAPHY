5eebd6894e6e463e4bc34c1118dc618840ccff67
Srijaswi-N <137861761+Srijaswi-N@users.noreply.github.com>
2023-07-28 19:58:29 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,31 @@
+ReactDOM.render(
+<div>
+     <h1><i>A P J ABDUL KALAM</i></h1>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/A._P._J._Abdul_Kalam.jpg/1200px-A._P._J._Abdul_Kalam.jpg" height="300px" width="300px"></img>
+
+    <h3><i>Full Name</i>      :AVUL PAKIR JAINULABDEEN ABDUL KALAM</h3>
+    <h3><i>Born </i>          :October 15,1931</h3>
+    <h3><i>Resting Place</i>  :Rameswaram,Tamil Nadu,India</h3>
+    <h3><i>Parents </i>      :Jainulabiddin Marakayar,Ashiamma Jainulabiddin</h3>
+    <h3><i>Profession </i>    :Aerospace Scientist</h3>
+    <h2>Elected as 11th President of India in 2002</h2>
+    <h2>Awards</h2>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/4/4c/The_President%2C_Dr._A._P._J._Abdul_Kalam_presenting_the_Padma_Shri_Award_to_Dr._%28Smt.%29_Sudha_Murthy.jpg" height="150px" width="150px"></img>
+    <ul>
+        <li>Padma Bhushan</li>
+        <li>Padma Vibhushan</li>
+        <li>Bharat Ratna</li>
+        <li>Veer Savarkar Award</li>
+    </ul>
+    <h2>Books</h2>
+    <img src="https://upload.wikimedia.org/wikipedia/en/3/3a/Wings_of_Fire_by_A_P_J_Abdul_Kalam_Book_Cover.jpg" height="150px" width="150px"></img>
+    <ul>
+        <li>Wings Of Fire</li>
+        <li>Indomitable Spirits</li>
+        <li>Ignited Minds</li>
+        <li>Turning Points</li>
+        <li>My Journey </li>
+    </ul>
+    <h3><i>Died :27 July,2015</i></h3>
+    </div>,
+    document.getElementById("content"));
